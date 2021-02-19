d53874468f0a6721039b6de63c86a2d5d1ab09b0
PiotrGrzechnik <ppgrzechnik@gmail.com>
2021-02-19 19:37:23 +0100

    Basic configuration, react, webpack, typescript, eslint, styled-components, antd

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Main from "./views/Main";
+import "antd/dist/antd.css";
+
+const App: React.FC = () => <Main />;
+
+export default App;
---
+++ src/views/Main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background: #0055f3;
+`;
+
+const Main: React.FC = () => <Container>Main</Container>;
+
+export default Main;
