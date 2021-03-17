19638a23005d0c290ef02c920cb3f715d1a12c1e
PiotrGrzechnik <ppgrzechnik@gmail.com>
2021-03-17 20:25:51 +0100

    Add custom hooks

--- src/data/react/hooks/basic.ts
+++ src/data/react/hooks/basic.ts
@@ -37,4 +37,69 @@ if (isMounted.current) {
 			},
 		],
 	},
+	{
+		category: Category.REACT,
+		subcategory: ReactType.HOOKS,
+		title: 'Keep previous value',
+		icon: 'las la-undo-alt',
+		codes: [
+			{
+				key: '1',
+				code: `
+const usePrevious = (value) => {
+	const ref = React.useRef()
+
+	React.useEffect(() => {
+		ref.current = value
+	})
+
+	return ref.current
+}
+      `,
+				usage: `
+// In component:
+
+const lastValue = usePrevious(value)
+      `,
+			},
+		],
+	},
+	{
+		category: Category.REACT,
+		subcategory: ReactType.HOOKS,
+		title: 'Mouse position hook',
+		icon: 'las la-mouse-pointer',
+		codes: [
+			{
+				key: '1',
+				code: `
+const useMousePosition = () => {
+	const [mousePosition, setMousePosition] = useState({})
+
+	useEffect(() => {
+		const getMousePosition = e => {
+			const posX = e.clientX
+			const posY = e.clientY
+			setMousePosition({ posX, posY })
+		}
+
+		document.addEventListener("mousemove", getMousePosition)
+
+		return function cleanup() {
+			document.removeEventListener("mousemove", getMousePosition)
+		}
+	})
+
+	return mousePosition
+}
+      `,
+				usage: `
+// In component:
+
+const { posX, posY } = useMousePosition()
+
+      `,
+			},
+		],
+	},
 ];
